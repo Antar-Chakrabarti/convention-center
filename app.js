e47156e4971074dcbabc81db3049a2a95badb5ec
Antar-chakrabarti <chakrabartiantar@gmail.com>
2022-06-01 14:08:15 +0600

    added some text

---
+++ app.js
@@ -0,0 +1,16 @@
+/**
+ * 1. find the link of the private repo of your assignment
+ * 2. create a folder
+ * 3. open cmd and cd to that folder [my assignment]
+ * 4. git clone --bare <private repo .git link>
+ * 5. cd to the folder of the assignment .git folder
+ * 6. go to github .com and create a public repo
+ * 7. git push --mirror < public repo .git link >
+ * 
+ * -------------
+ * push code ot the public repo from assignment folder of your computer
+ * 1. git remote set-url origin < public repo .git link>
+ * -------------
+ * alternative:
+ * 1. clone your public repo
+*/
